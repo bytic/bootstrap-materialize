e24a9c9f370a779df677d8021641879a5934b257
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-04-18 15:29:45 +0300

    update pallete

---
+++ README.md
@@ -0,0 +1,15 @@
+# Material Design for Bootstrap 4
+
+
+
+________
+
+#### Inspiration
+
+- https://github.com/FezVrasta/bootstrap-material-design
+
+- https://github.com/mdbootstrap/bootstrap-material-design
+
+- https://github.com/Daemonite/material
+
+- [Premium Templates](https://mdbootstrap.com/templates/)
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// 'use strict';
+
+// webpack.config.js
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = require("./build/webpack/config");
+
+const fontsRule = require("./build/webpack/rules/fonts");
+const sassRule = require("./build/webpack/rules/sass");
+const lessRule = require("./build/webpack/rules/less");
+const imagesRule = require("./build/webpack/rules/images");
+
+module.exports = {
+  entry: {
+    'materialize': [
+      path.join(__dirname, 'scss/materialize.scss'),
+      path.join(__dirname, 'js/materialize.js')
+    ]
+  },
+
+  /**
+   * Output settings for application scripts.
+   * @type {Object}
+   */
+  output: {
+    path: config.paths.public,
+    publicPath: config.outputs.publicPath,
+    filename: config.outputs.javascript.filename,
+    chunkFilename: '[name].js'
+  },
+
+  module: {
+    rules: [
+      lessRule,
+      sassRule,
+      fontsRule,
+      imagesRule,
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: "css-loader",
+            options: {importLoaders: 1},
+          }],
+        })
+      }
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin(config.outputs.css),
+  ],
+  stats: "normal"
+};
