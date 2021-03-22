beeea4c827d7e5c9a870b93c2deffcaee17af44d
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-03-22 17:43:00 +0200

    add form row spacers

--- dist/js/materialize-vendors.js
+++ dist/js/materialize-vendors.js
@@ -94,7 +94,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var bootstrap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bootstrap */ \"./node_modules/bootstrap/dist/js/bootstrap.esm.js\");\n\n\n\n//# sourceURL=webpack:///./js/materialize-vendors.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var bootstrap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bootstrap */ \"./node_modules/bootstrap/dist/js/bootstrap.esm.js\");\n\r\n\n\n//# sourceURL=webpack:///./js/materialize-vendors.js?");
 
 /***/ }),
 
