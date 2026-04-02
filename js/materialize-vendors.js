a52c818d47ac59980bc75da862e919a8299a7a6c
Gabriel Solomon <hello@gabrielsolomon.ro>
2026-04-02 21:20:16 +0300

    tag: 5.0.5

--- js/materialize-vendors.js
+++ js/materialize-vendors.js
@@ -1,1 +1,77 @@
 import 'bootstrap';
+// import * as Popper from "@popperjs/core"
+// import 'bootstrap';
+//
+// // Import Bootstrap 5 JavaScript components (only those actively used)
+// import {
+//   Alert,
+//   Button,
+//   Carousel,
+//   ScrollSpy,
+//   Collapse,
+//   Dropdown,
+//   Modal,
+//   Offcanvas,
+//   Popover,
+//   Tab,
+//   Toast,
+//   Tooltip,
+// } from 'bootstrap';
+//
+// $(document).ready(function () {
+//   console.log('Hello, world!');
+//   document.querySelectorAll('.dropdown-toggle').forEach(element => {
+//     console.log(element);
+//     var drop = new Dropdown(element);
+//     console.log(drop);
+//   });
+//
+//   document.querySelectorAll('.alert').forEach(element => {
+//     new Alert(element);
+//   });
+//
+//   document.querySelectorAll('.btn').forEach(element => {
+//     new Button(element);
+//   })
+//
+//   document.querySelectorAll('.carousel').forEach(element => {
+//     new Carousel(element);
+//   })
+//
+//   document.querySelectorAll('.scrollspy').forEach(element => {
+//     new ScrollSpy(element);
+//   })
+// // Initialize modals
+//   document.querySelectorAll('.modal').forEach(element => {
+//     new Modal(element);
+//   });
+//
+// // Initialize offcanvas
+//   document.querySelectorAll('.offcanvas').forEach(element => {
+//     new Offcanvas(element);
+//   });
+//
+// // Initialize collapse elements
+//   document.querySelectorAll('[data-bs-toggle="collapse"]').forEach(element => {
+//     new Collapse(element);
+//   });
+//
+// // Initialize tabs
+//   document.querySelectorAll('[data-bs-toggle="tab"]').forEach(element => {
+//     new Tab(element);
+//   });
+//
+// // Initialize toasts
+//   document.querySelectorAll('.toast').forEach(element => {
+//     new Toast(element);
+//   });
+// // Initialize tooltips
+//   document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(element => {
+//     new Tooltip(element);
+//   });
+//
+// // Initialize popovers
+//   document.querySelectorAll('[data-bs-toggle="popover"]').forEach(element => {
+//     new Popover(element);
+//   });
+// });
